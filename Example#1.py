0d9377b2df84a24ab43865eb7531bdaec92ff4d1
mschen-sta <cherrie.chen@ycdsb.ca>
2024-04-18 14:52:27 -0400

    Updated

--- Example#1.py
+++ Example#1.py
@@ -7,6 +7,11 @@
 
 # Example #1:
 ## Part (a) Create a function that will calculate the area of a rectangle
+length = float(input("Enter the length (in cm): "))
+width = float(input("Enter the width (in cm): "))
 
+area = length * width
+
+print(f"The area of the rectangle is: {area} cm^2")
 
 ## Part (b) Use your function by calling it
